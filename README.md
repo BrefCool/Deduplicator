28b5e4c7e606c2d7689a775b86b1084f3f4ac3e6
br3fc00l <461761446@qq.com>
2019-04-13 14:20:21 -0400

    update backup from bitbucket

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Deduplicator
+
+## How to use dedup command?
+go to the `release` folder, there are two files: `dedup` and jar file `deduplicator.jar`.  
+then use  
+```
+chmod +x dedup
+```
+make dedup executable.  
+
+
+add the path of dedup into the `PATH` environment variable.  
+```
+export PATH=$PATH:/home/yuxuansu/study/EC504/Deduplicator/release
+```
+
+then you can use the dedup command to add file or retrieve file  
+```
+dedup -addFile <filename> -locker <lockername>
+dedup -retrieveFile <filename> -output <file saved path> -locker <lockername>
+```
+
+all the chunks and metadata will be stored into `$HOME/.dedupStore` directory.
--- deduplicator/src/edu/bu/ec504/group9/GUI.java
+++ deduplicator/src/edu/bu/ec504/group9/GUI.java
@@ -8,17 +8,26 @@ public class GUI extends JFrame {
 
     public  GUI() {
         FileIO.initialize();
+        Locker myLocker = LockerFactory.getLocker("myLocker", LockerFactory.CHUNKING.FIXEDSIZE);
+
         JFrame frame=new JFrame("DeDuplicator");
         Box b1=Box.createHorizontalBox();  // create box
         frame.add(b1);  // add box
         b1.add(Box.createVerticalStrut(300));  //set vertical structure
+
         JButton AFB = new JButton("Add File");  // add addfile button
+        JButton RB = new JButton("Retrieve File");
+        JButton RFI = new JButton("Retrieve FileInfo");
+
         b1.add(AFB);  // add button to box
+        b1.add(RB);
+        b1.add(RFI);
         b1.add(Box.createHorizontalStrut(200));
         b1.add(Box.createHorizontalGlue());
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // set exit function
+
         AFB.addActionListener(ActionEvent-> {       // choose file
-            Locker myLocker = LockerFactory.getLocker("myLocker", LockerFactory.CHUNKING.FIXEDSIZE);
             JFileChooser jfc = new JFileChooser();
             jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
             jfc.showDialog(new JLabel(), "Choose");
@@ -28,6 +37,19 @@ public class GUI extends JFrame {
             }
         });
 
+        RB.addActionListener(ActionEvent-> {       // retrieve file
+            JFileChooser jfc = new JFileChooser();
+            jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            jfc.showDialog(new JLabel(), "Choose");
+            File file = jfc.getSelectedFile();
+            if (null != file) {
+                myLocker.retrieveFile("TestFile00.txt",file.getAbsolutePath());
+            }
+        });
+
+        RFI.addActionListener(ActionEvent-> {       // retrieve file
+            System.out.println(myLocker.retrieveFileInfo());
+        });
         frame.setBounds(100,100,400,200);
         frame.setVisible(true);
     }
